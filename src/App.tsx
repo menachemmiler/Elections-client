f242e620703b62bd21412d8cb90be1165fee3b10
menachem <AM0556727820@GMAIL.COM>
2024-11-19 11:52:28 +0200

    dcaca

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,28 @@ import Votes from "./components/pages/Votes";
 import Statistics from "./components/pages/Statistics";
 import { useEffect } from "react";
 import { socket } from "./main";
-import { useAppDispatch } from "./redux/store";
+import { useAppDispatch, useAppSelector } from "./redux/store";
 import { updateCandidates } from "./redux/slices/candidatesSlice";
 import SettingsPage from "./components/pages/SettingsPage";
 
-
-
-
-
-
 export default function App() {
   const dispatch = useAppDispatch();
+  const { theme } = useAppSelector((state) => state.settings);
 
-    useEffect(() => {
+  useEffect(() => {
     socket.on("updateCandidate", (newCandidate) => {
-      dispatch(updateCandidates(newCandidate)); 
+      dispatch(updateCandidates(newCandidate));
     });
+    console.log({ theme });
   }, []);
 
-
   return (
-    <div>
+    <div className={`${theme}`}>
       <Nav />
       <Routes>
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
-        <Route path="setting" element={<SettingsPage/>} />
+        <Route path="setting" element={<SettingsPage />} />
         <Route path="votes" element={<Votes />} />
         <Route path="statistics" element={<Statistics />} />
         <Route path="/" element={<Navigate to={"/login"} />} />
