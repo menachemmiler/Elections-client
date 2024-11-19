c518aa532498fec0357a6bde76f9a192f59e8217
menachem <AM0556727820@GMAIL.COM>
2024-11-19 16:11:03 +0200

    bla

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: {
+        jsx: 'react-jsx',        
+        esModuleInterop: true,   
+        strict: true
+      }
+    }]
+  }
+};
+
+export default config;
